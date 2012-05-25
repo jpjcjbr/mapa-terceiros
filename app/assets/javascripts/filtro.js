8794dcd73eebcc36f6921934a56ec1ebfe859f1f
João Paulo <jpjcjbr@gmail.com>
2012-05-25 19:05:13 -0300

    novo layout

---
+++ app/assets/javascripts/filtro.js
@@ -0,0 +1,11 @@
+function filter (phrase, _id, cellNr){
+  var suche = phrase.value.toLowerCase();
+  var table = document.getElementById(_id);
+  var ele;
+  for (var r = 1; r < table.rows.length; r++){
+        ele = table.rows[r].cells[cellNr].innerHTML.replace(/<[^>]+>/g,"");
+        if (ele.toLowerCase().indexOf(suche)>=0 )
+    table.rows[r].style.display = '';
+        else table.rows[r].style.display = 'none';
+  }
+        }
