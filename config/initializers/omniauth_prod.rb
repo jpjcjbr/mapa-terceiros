0543c6523a807fea86b045b1095173a191d2d31d
JP <jpjcjbr@gmail.com>
2012-11-12 11:28:38 -0200

    correcao da redirect uri

--- config/initializers/omniauth_prod.rb
+++ config/initializers/omniauth_prod.rb
@@ -3,7 +3,7 @@ OmniAuth.config.logger = Rails.logger
 if Rails.env == 'production'
   Rails.application.config.middleware.use OmniAuth::Builder do
     provider :vpsa, "50533a04d93a4b7a7c00000d", "3463798f6a4c10b1b998634f7c3b3fff713362abaf6153bd428b0219e6144e1d",  {
-      redirect_uri:'http://mapa-terceiros.herokuapp.com/oauth/vpsa/callback'
+      redirect_uri:'http://mapa-terceiros.herokuapp.com/auth/vpsa/callback'
     }
   end
 end
